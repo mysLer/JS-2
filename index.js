881e0e8abdfa13a8e75973b319d250e14b0f168e
mnd <mehmetndincer@gmail.com>
2023-04-10 12:09:39 +0200

    var-let-const

---
+++ index.js
@@ -0,0 +1,98 @@
+/* console.log("Hello World!")
+
+var userName = "Gamze"
+
+userName = "Ebuzer"
+
+console.log(userName)
+
+let mail ="abc@gmail.com"
+
+mail="xyz" 
+
+console.log(mail)
+
+let -> mutable (degistirilebilir degisken, yani tekrar let ile baslayan bir mail yamamam)
+
+console.log(mail)
+
+mail = "xyz@gmail.com"
+
+console.log(mail)
+
+const email = "ababab"
+
+const -> immutable (degistirilemez degisken)
+
+let uName="Afra";
+
+const secondName;   ( yani bu sekilde hata verir, const un yanina yeni bir veri atayamassin)
+
+let firstUserName;
+let user1Name;
+let _name;
+let $name;
+let leri olustururken bu sekilde olusturabiliriz)
+
+JS te noktalivirgul koymak zorunda degiliz, ama satirin bittigini belli etmek icin koymakta fayda var
+
+let a = 3;
+let b = 8;
+
+burada a yi 8 ´, b yi ise 3 nasil yapariz sorusuna cevap asagida
+let c = a
+a = b
+b = c
+
+console.log(a,b) */
+/*
+this is used for
+multiline comments
+*/
+
+var fName = "Afra"
+let fName2 = "Muhlise"  /*yeni isim verilebiliyor*/
+fName2 = "Kemal"    /*yeni isim verilebiliyor*/
+const fName3 = "Ebuzer"
+/* fName3 = "Ahmet" */  /*yeni isim VerileMEZ*/
+
+
+
+// ---- DATA  TYPES ---- // 
+
+/* 
+1. string
+2. number
+3. boolean   (dogru yanlis degerlere diyoruz-true-false)
+4. undefined (tirnak isaretleri, atama yapmadim sadece tanimlama yaptim diyorsak bu yazar)
+5. null (atama yaptim ama bos kalsin sonra doldurcam diyorsak null diyoruz)
+
+ */
+
+
+let name = "Afra" 
+
+let phoneNummer ;
+console.log("phoneNummer",phoneNummer)
+
+let email = null;
+console.log("email",email)
+
+let variable = 'ali29';
+
+console.log(typeof variable)
+
+console.log(email)
+
+console.log(phoneNummer)
+
+console.log(typeof name)
+
+console.log(typeof "123")
+
+console.log(typeof 3.14)
+
+console.log(typeof true)
+
+console.log(typeof false)
+
